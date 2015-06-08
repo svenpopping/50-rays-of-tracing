a013fa4874d6b92135ac175733040cc80673dfaf
Jan-Willem Manenschijn <janwillem@q42.nl>
2015-06-09 00:54:14 +0200

    Added support for mac, created a config file for loading the .obj

---
+++ default.config.h
@@ -0,0 +1,20 @@
+//
+//  default.config.h
+//  Raytracer
+//
+//  Created by Jan-Willem Manenschijn on 08/06/15.
+//  Copyright (c) 2015 Group 57. All rights reserved.
+
+//
+//  COPY THIS FILE AND NAME IT config.h
+//  SET YOUR OWN FULL PATH TO THE FILE
+//  DO NEVER CHECK config.h IN ON GITHUB!
+//
+
+//
+
+#ifndef Raytracer_config_h
+#define Raytracer_config_h
+static const char *FILE_LOCATION = "YOUR FILE LOCATION HERE";
+
+#endif
