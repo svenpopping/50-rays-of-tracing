bd3533d5d4c86136ec565535a655e555896b948d
Jan-Willem Manenschijn <janwillem@q42.nl>
2015-06-15 14:35:19 +0200

    Added a helper file with helper functions for programming in c++. First added function is printing a vertex and vector!

---
+++ include/helper.h
@@ -0,0 +1,20 @@
+//
+//  helper.h
+//  Raytracer
+//
+//  Created by Jan-Willem Manenschijn on 15/06/15.
+//  Copyright (c) 2015 Group 57. All rights reserved.
+//
+
+
+#ifndef Raytracer_helper_h
+#define Raytracer_helper_h
+
+
+#include "Vec3D.h"
+#include <iostream>
+
+void printVector(Vec3Df vector);
+void printVertex(Vertex vertex);
+
+#endif
