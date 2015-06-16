0ea3d975a83ad7a08c8bc3b12e8d615364ad95ed
Jan-Willem Manenschijn <janwillem@q42.nl>
2015-06-16 19:27:11 +0200

    Added the header for the clearDebugVector method

--- include/raytracing.h
+++ include/raytracing.h
@@ -39,6 +39,7 @@ void yourDebugDraw();
 
 //want keyboard interaction? Here it is...
 void yourKeyboardFunc(char t, int x, int y, const Vec3Df & rayOrigin, const Vec3Df & rayDestination);
+void clearDebugVector();
 
 
 // Shading functions
